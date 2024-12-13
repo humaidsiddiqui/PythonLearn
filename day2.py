5d80192d350a46ed2834ab6496b3e673e7fa50f5
humaidsiddiqui <humaidsiddiqui@gmail.com>
2024-12-12 19:32:23 -0800

    input from the users

---
+++ day2.py
@@ -0,0 +1,8 @@
+# input statement input() is used to accept values (using keyword) from user
+name=input("please my name is")
+age=int(input("enter your age"))
+university=(input("enter your university"))
+
+print(name,age,university)
+
+
