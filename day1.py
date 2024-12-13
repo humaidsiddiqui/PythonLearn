741b0cf05554c8304ee17629a779a7863020c643
humaidsiddiqui <humaidsiddiqui@gmail.com>
2024-12-12 18:53:02 -0800

    some basic and starting

---
+++ day1.py
@@ -0,0 +1,6 @@
+print("hello world!")
+
+name = "Humaid siddiqui"
+age = 23
+university = "Fairleigh Dickinson University"
+print("my name is",name,"and my age is",age,"and I'm studying at", university)
