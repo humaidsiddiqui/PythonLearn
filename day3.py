87dcaaa8f6a8b7e0b2f0cabd5d52920ccc3625db
humaidsiddiqui <humaidsiddiqui@gmail.com>
2024-12-12 21:59:21 -0800

    if-elif-else and clever

--- day3.py
+++ day3.py
@@ -8,3 +8,21 @@ elif (light=="yellow"):
     print("LOOKUP!")
 else:
     print("the light is broken")
+
+
+# if-elif-else statement in one line
+food = input ("enter the food you want to eat : ")
+eat = "yes" if food == "cake" else "no"
+print(eat)
+
+
+# clever if / ternary operator
+age = int(input("enter your age = "))
+vote = ("yes", "no") [age >= 18] 
+print(vote)
+
+
+sal=int(input("enter your salary"))
+tax = sal*(0.1, 0.2) [sal<=50000] 
+print(tax)
+
