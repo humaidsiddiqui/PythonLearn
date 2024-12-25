5fbd83863279bffda21f8058cda4c117d590c510
humaidsiddiqui <humaidsiddiqui@gmail.com>
2024-12-24 23:35:00 -0800

    added to 2 lines of code

---
+++ Projects/password_manager.py
@@ -0,0 +1,11 @@
+pwd = input("what is the master password? ")
+
+while True:
+    mode = input("what you like to add a new or view existing ones (view,add) ?")
+    if mode == "view":
+        pass
+    elif mode=="add":
+        pass
+    else:
+        print("invalid mode")
+        exit()
