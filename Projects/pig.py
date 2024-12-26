195bbc4c568aed552826cd5b6cdeecba0a599fa1
humaidsiddiqui <humaidsiddiqui@gmail.com>
2024-12-26 01:41:12 -0800

    pig game

--- Projects/pig.py
+++ Projects/pig.py
@@ -1,46 +1,54 @@
 import random
 
+
 def roll():
-    min_value=1
-    max_value=6
-    roll=random.randint(min_value,max_value)
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+
     return roll
 
+
 while True:
-    players=input("enter the number for players (2-4): ")
-    if(players.isdigit()):
-        players=int(players)
-        if(2 <= players <= 4):
+    players = input("Enter the number of players (2 - 4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
             break
         else:
-            print("Must be between 2 to 4 players !")
+            print("Must be between 2 - 4 players.")
     else:
-        print("invalid Try Again!")
-        continue
-max_score=50
-players_score=[0 for _ in range(players)] # this is list comprehension where it will add 0 for score in total no.of players!
-print(players_score)
+        print("Invalid, try again.")
 
-current_score=0
+max_score = 50
+player_scores = [0 for _ in range(players)]
 
-while max(players_score)<max_score:
+while max(player_scores) < max_score:
+    for player_idx in range(players): # this is list comprehension where it will add 0 for score in total no.of players!
+        print("\nPlayer number", player_idx + 1, "turn has just started!")
+        print("Your total score is:", player_scores[player_idx], "\n")
+        current_score = 0
 
-    for player_idx in range(players):
-        current_score=0
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != "y":
+                break
 
-    while True:
-        should_roll=input("would you like to roll ? (Y)? ").lower()
-        if(should_roll!="y"):
-            break
-        value=roll()
-        if value == 1:
-            print("you rolled the one! turned done")
-        else:
-            current_score+=value
-            print("you rolled a: ",value)
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You rolled a:", value)
 
-        players_score[player_idx] += current_score
-        print("your current scoire is :",player_idx[players_score])
+            print("Your score is:", current_score)
 
+        player_scores[player_idx] += current_score
+        print("Your total score is:", player_scores[player_idx])
 
-
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number", winning_idx + 1,
+      "is the winner with a score of:", max_score)
